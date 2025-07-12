693f5b58f434ed55e6052cb679b6adadb6133dff
MacYuki <vicfarth247@gmail.com>
2025-07-12 19:22:50 +0900

    ページの追加とホームページの見た目の調整

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'second_page.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -29,20 +31,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  List<String> entries = <String>['無題'];
+  List<String> entries = <String>["A","B","C"];
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     return Scaffold(
       appBar: AppBar(
         title: Text(
-                "all",
-                style: TextStyle(
-                  fontSize: 30,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-              ),
+          "all",
+          style: TextStyle(
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+        ),
         actions: [
           IconButton(
             onPressed: () {
@@ -62,63 +64,100 @@ class _MyHomePageState extends State<MyHomePage> {
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.all(20.0),
-                child: Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.grey,
-                      width: 1,
-                    ),
-                    borderRadius: BorderRadius.circular(13)
-                  ),
-                  child: ListView.separated(
-                    itemCount: entries.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      List<Widget> children = [];
-                      if (index == 0) {
-                        children.add(Divider());
-                      }
-                      children.add(Choices(entries: entries, index: index));
-                      if (index == entries.length - 1) {
-                        children.add(Divider());
-                      }
-                      return Column(children: children);
-                    },
-                    separatorBuilder: (BuildContext context, int index) =>
-                        const Divider(),
-                  ),
+                child: ListView.separated(
+                  itemCount: entries.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    List<Widget> children = [];
+                    if (index == 0) {
+                      children.add(
+                        Column(
+                          children: [
+                            Container(
+                              height: 10,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: const BorderRadius.only(
+                                  topLeft: Radius.circular(20),
+                                  topRight: Radius.circular(20),
+                                ),
+                              ),
+                            ),
+                            Container(height: 1, color: Colors.grey),
+                          ],
+                        ),
+                      );
+                    }
+                    children.add(
+                      GestureDetector(
+                        onHorizontalDragEnd: (details){
+                          if (details.velocity.pixelsPerSecond.dx < 0){
+                            setState((){
+                              entries.removeAt(index);
+                            });
+                            print("swipe");
+                          }
+                        },
+                        onTap: () {
+                          Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => SecondPage()),
+                        );
+                        },
+                        child: Container(
+                          color: Colors.white,
+                          height: 70,
+                          width: double.infinity,
+                          child: Padding(
+                            padding: const EdgeInsets.only(left:15),
+                            child: Text(
+                              entries[index],
+                              style: TextStyle(
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                                color: Colors.black,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                    if (index == entries.length - 1) {
+                      children.add(
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: const BorderRadius.only(
+                              bottomLeft: Radius.circular(20),
+                              bottomRight: Radius.circular(20),
+                            ),
+                          ),
+                          child: Column(
+                          children: [
+                            Container(height: 1, color: Colors.grey),
+                            Container(
+                              height: 10,
+                              decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius: const BorderRadius.only(
+                                  bottomLeft: Radius.circular(20),
+                                  bottomRight: Radius.circular(20),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        ),
+                      );
+                    }
+                    return Column(children: children);
+                  },
+                  separatorBuilder: (BuildContext context, int index) =>
+                      Container(height: 1, color: Colors.grey) 
+                      ,
                 ),
               ),
             ),
           ],
-        ),
-      ),
-    );
-  }
-}
-
-class Choices extends StatelessWidget {
-  const Choices({
-    super.key,
-    required this.index,
-    required this.entries,
-  });
-
-  final int index;
-  final List<String> entries;
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: (){
-        
-      },
-      child: SizedBox(
-        height: 70,
-        child: Center(
-          child: Text(
-            entries[index],
-            style: TextStyle(color: Colors.black),
-          ),
         ),
       ),
     );
